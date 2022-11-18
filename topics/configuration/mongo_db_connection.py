2e9d9682a11776b178acd5ad31ae58dd1e03bc4d
Shyam-AI <shyamdl2803@gmail.com>
2022-11-19 00:52:03 +0530

    updated structure

---
+++ topics/configuration/mongo_db_connection.py
@@ -0,0 +1,23 @@
+import pymongo
+from topics.constant.database import DATABASE_NAME
+import certifi
+import os
+ca = certifi.where()
+
+class MongoDBClient:
+    client = None
+    def __init__(self, database_name= DATABASE_NAME) -> None:
+        try:
+
+            if MongoDBClient.client is None:
+                mongo_db_url = "mongodb+srv://shyammongo:shyammongo@cluster0.t9auzoz.mongodb.net/?retryWrites=true&w=majority"  #os.getenv(MONGODB_URL_KEY)
+                print(mongo_db_url)
+                if "localhost" in mongo_db_url:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
+                else:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+        except Exception as e:
+            raise e
