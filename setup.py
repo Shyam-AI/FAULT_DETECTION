4babb4ca70b491e98333af2eb7b00b88b0db6eda
Shyam-AI <shyamdl2803@gmail.com>
2022-11-19 00:51:18 +0530

    fixed setup installation

--- setup.py
+++ setup.py
@@ -1,9 +1,32 @@
+# from setuptools import find_packages,setup
+# from typing import List
+
+# def get_requirements()->List[str]:
+#     """
+#     This function will return list of requirements
+#     """
+#     requirement_list:List[str] = []
+
+#     """
+#     Write a code to read requirements.txt file and append each requirements in requirement_list variable.
+#     """
+#     return requirement_list
+
+# setup(
+#     name="sensor",
+#     version="0.0.1",
+#     author="Shyam U",
+#     author_email="shyamdl2803@gmail.com",
+#     packages = find_packages(),
+#     install_requires=get_requirements(),#["pymongo==4.2.0"],
+# )
+
 from setuptools import find_packages, setup
 from typing import List
+# list_of_requirements = [pymongo[srv]==4.2.0,certifi,-e .]
 
-
-def get_requirements()->List[str]:
-    requirement_list:List[str] = []
+def get_requirements()->List:
+    requirement_list:List = []
     try:
         req_file = open("requirements.txt")
         file_contents= req_file.read()
@@ -19,5 +42,5 @@ setup (
     author = "Shyam",
     author_email="shyamdl2803@gmail.com",
     packages=find_packages(),
-    install_requires = get_requirements(),
+    install_requires = get_requirements()
 )
