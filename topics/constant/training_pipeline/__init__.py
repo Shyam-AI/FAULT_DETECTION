b63b7e16ec45fb76b63a5067beb1e3b8483865cd
Shyam-AI <shyamdl2803@gmail.com>
2022-11-25 00:41:24 +0530

    pipeline constants updated

---
+++ topics/constant/training_pipeline/__init__.py
@@ -0,0 +1,70 @@
+import os
+from topics.constant.s3_bucket import TRAINING_BUCKET_NAME
+
+
+SAVED_MODEL_DIR =os.path.join("saved_models")
+#DEFINING TRAINING PIPELINE CONSTANTS
+
+FILE_NAME = "sensor.csv"
+PIPELINE_NAME: str = "sensor"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+SCHEMA_DROP_COLUMNS = "drop_columns"
+TARGET_COLUMN = "class"
+
+ARTIFACT_DIR: str = "artifact"
+
+PREPROCESSING_OBJ_FILE_NAME = "preprocessing.pkl"
+MODE_NAME: str = "model.pkl"
+
+
+
+"""
+    Data Ingestion related constants
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "sensor"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float=0.2
+
+"""
+Data validation constants
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "valid"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+"""
+Data Transformation ralated constant start with DATA_TRANSFORMATION VAR NAME
+"""
+
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
+
+"""
+Model Trainer ralated constant start with MODE TRAINER VAR NAME
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+
+"""
+Model Trainer related constant start with MODE TRAINER VAR NAME
+"""
+MODEL_EVALUATION_DIR_NAME: str = "model_evaluation"
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_EVALUATION_REPORT_NAME= "report.yaml"
+
+
+MODEL_PUSHER_DIR_NAME = "model_pusher"
+MODEL_PUSHER_SAVED_MODEL_DIR = SAVED_MODEL_DIR
