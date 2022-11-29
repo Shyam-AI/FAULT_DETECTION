e9cf573b61dc57ad81d44c02fcaa46ad3f58fdc4
Shyam-AI <shyamdl2803@gmail.com>
2022-11-29 11:39:04 +0530

    updated

--- aps_sensor/exception.py
+++ aps_sensor/exception.py
@@ -13,7 +13,7 @@ def error_message_detail(error, error_detail: sys):
     return error_message
 
 
-class TopicsException(Exception):
+class SensorException(Exception):
     def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_message = error_message_detail(
